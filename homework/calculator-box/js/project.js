ffcdb2080b609ffc15c371ba4967003ac2ca6a17
lilyreinhold <lily.reinhold@gmail.com>
2015-12-01 20:06:21 -0500

    homeworkandclasswork

---
+++ homework/calculator-box/js/project.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	var total = 0;
+}
+//value reassignment
+	$("#a10").click(function(){
+		total += 10;
+		//reassigning the value of x, adds the value of X to 0
+		$("#out").html(total);
+	});
+	$("#a20").click(function(){
+		total += 20;
+		$("#out").html(total);
+	});
+	$("#a30").click(function (){
+		total += 30;
+		$("#out").html(total);
+	});
+	$("#n10").click(function(){
+		total -= 10;
+		$("#out").html(total);
+	});
+	$("#n20").click(function(){
+		total -= 20;
+		$("#out").html(total);
+	});
+	$("#n30").click(function(){
+		total -= 30;
+		$("#out").html(total);
+	});
+	$("#red").click(function(){
+		$("#out").css("background-color", "red");
+	)};
+	$("#blue").click(function (){
+		$("#out").css("background-color"), "blue");
+	});
+	$("#out").click(function (){
+		$("#out").css("background-color", "white")
+	});
+});
